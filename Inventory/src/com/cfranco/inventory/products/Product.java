cc07e32a72e87c75896eb9fec3e6be8caa43e425
Claudio Franco <claudio.marques.franco@gmail.com>
2014-07-17 02:13:40 +0100

    Added Product class.
    Added .txt products, wich gives us the type of products that the reader
    will upload. Tv box insertion remaining and need to check the rest
    of the products in stock.

---
+++ Inventory/src/com/cfranco/inventory/products/Product.java
@@ -0,0 +1,34 @@
+package com.cfranco.inventory.products;
+
+public class Product {
+	private int serialNumberLenght;
+	private String productName;
+	
+	public Product(int serialNumberLength, String productName) {
+		this.serialNumberLenght = serialNumberLength;
+		this.productName = productName;
+	}
+	
+	/**
+	 * Returns the length of the serial Number. Return -1 if no serial has been given 
+	 * 
+	 * */
+	
+	public int getLength(){
+		if(serialNumberLenght != 0){
+			return serialNumberLenght;
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * Returns the name of the product.
+	 * 
+	 * */
+	
+	public String getName(){
+		return productName;
+	}
+
+}
