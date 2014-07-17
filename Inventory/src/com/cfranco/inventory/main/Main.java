1bf3755fa9a028f6e7ffe98ba44a5b4bd222d6cf
Claudio Franco <claudio.marques.franco@gmail.com>
2014-07-17 01:50:58 +0100

    First change
    Add a simple input change

--- Inventory/src/com/cfranco/inventory/main/Main.java
+++ Inventory/src/com/cfranco/inventory/main/Main.java
@@ -3,5 +3,6 @@ package com.cfranco.inventory.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test inventory");
+		System.out.println("1st change!");
 	}
 }
